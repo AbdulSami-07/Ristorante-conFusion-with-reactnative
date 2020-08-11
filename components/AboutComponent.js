e6078587a5c57334810f73a5e28587824629830d
AbdulSami-07 <sami07israil@gmail.com>
2020-08-12 00:16:23 +0530

    Icons and Buttons

---
+++ components/AboutComponent.js
@@ -0,0 +1,67 @@
+import React , { Component } from 'react';
+import { Card, ListItem, Text } from 'react-native-elements';
+import { ScrollView, FlatList, SafeAreaView } from 'react-native';
+import { LEADERS } from '../shared/leaders';
+
+function History(props){
+    return(
+        <Card>
+            <Text style={{fontWeight: 'bold',textAlign: 'center'}}>
+            Our History
+            {'\n\n'}
+            </Text>
+            <Text>
+            Started in 2010, Ristorante con Fusion quickly established itself as a culinary icon par excellence in Hong Kong. With its unique brand of world fusion cuisine that can be found nowhere else, it enjoys patronage from the A-list clientele in Hong Kong.  Featuring four of the best three-star Michelin chefs in the world, you never know what will arrive on your plate the next time you visit us.
+            {"\n\n"}
+            The restaurant traces its humble beginnings to The Frying Pan, a successful chain started by our CEO, Mr. Peter Pan, that featured for the first time the world's best cuisines in a pan.
+            </Text>
+        </Card>
+    );
+}
+
+class About extends Component{
+    
+    constructor(props){
+        super(props);
+        this.state = {
+            leaders : LEADERS
+        }
+    }
+
+    static navigationOptions = {
+        title : 'About us'
+    };
+
+    render(){
+        const renderMenuItem = ({item, index}) =>{
+            return (
+                <ListItem 
+                    key={index}
+                    title={item.name}
+                    subtitle={item.description}
+                    hideChevron={true} //to hide right arrow as used in ios
+                    leftAvatar={{source: require('./images/alberto.png')}}
+                />
+            );
+        };
+
+
+        return(
+            <SafeAreaView>
+                <ScrollView>
+                    <History />
+                    <Card title="Corporate Leadership">
+                        <FlatList
+                            data={this.state.leaders} // each element in array becomes item
+                            renderItem={renderMenuItem}
+                            keyExtractor={item => item.id.toString()} //keyExtractor take string as value. 
+                        />
+                    </Card>
+                </ScrollView>
+            </SafeAreaView>
+        );
+    }
+        
+}
+
+export default About;
