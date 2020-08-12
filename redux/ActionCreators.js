522d52fc9ffd5c72828abf44cf3548150f56deec
AbdulSami-07 <sami07israil@gmail.com>
2020-08-12 12:40:08 +0530

    Setting up Redux

---
+++ redux/ActionCreators.js
@@ -0,0 +1,163 @@
+import * as ActionTypes from './ActionTypes';
+import  { baseUrl } from '../shared/baseUrl';
+
+export const fetchComments = () => (dispatch) => {
+    return fetch(baseUrl + 'comments' )
+                .then(response =>{
+                    if(response.ok){
+                        return response;
+                    }
+                    else{
+                        //server error
+                        let error = new Error("Error: " + response.status + ": " + response.statusText);
+                        error.response = response;
+                        throw error;
+                    }
+                },error => { //in app error
+                    let  errMess = new Error(error.message);
+                    throw errMess;
+                })
+                .then(response => response.json())
+                .then(comments => dispatch(addComments(comments)))
+                .catch(error => dispatch(commentsFailed(error.message)));
+}
+
+export const addComments = (comments) => {
+    return ({
+        type: ActionTypes.ADD_COMMENTS,
+        payload: comments
+    });
+}
+
+export const commentsFailed = (errMess) => {
+    return({
+        type: ActionTypes.COMMENTS_FAILED,
+        payload: errMess
+    });
+}
+
+export const fetchDishes = () => (dispatch) => {
+    dispatch(dishesLoading());
+    return fetch(baseUrl + 'dishes' )
+                .then(response =>{
+                    if(response.ok){
+                        return response;
+                    }
+                    else{
+                        //server error
+                        let error = new Error("Error: " + response.status + ": " + response.statusText);
+                        error.response = response;
+                        throw error;
+                    }
+                },error => { //in app error
+                    let  errMess = new Error(error.message);
+                    throw errMess;
+                })
+                .then(response => response.json())
+                .then(dishes => dispatch(addDishes(dishes)))
+                .catch(error => dispatch(dishesFailed(error.message)));
+}
+
+export const addDishes= (dishes) => {
+    return ({
+        type: ActionTypes.ADD_DISHES,
+        payload: dishes
+    });
+}
+
+export const dishesFailed = (errMess) => {
+    return({
+        type: ActionTypes.DISHES_FAILED,
+        payload: errMess
+    });
+}
+
+export const dishesLoading = () => {
+    return ({
+        type: ActionTypes.DISHES_LOADING
+    });
+}
+
+export const fetchPromos = () => (dispatch) => {
+    dispatch(promosLoading());
+    return fetch(baseUrl + 'promotions' )
+                .then(response =>{
+                    if(response.ok){
+                        return response;
+                    }
+                    else{
+                        //server error
+                        let error = new Error("Error: " + response.status + ": " + response.statusText);
+                        error.response = response;
+                        throw error;
+                    }
+                },error => { //in app error
+                    let  errMess = new Error(error.message);
+                    throw errMess;
+                })
+                .then(response => response.json())
+                .then(promotions => dispatch(addPromos(promotions)))
+                .catch(error => dispatch(PromosFailed(error.message)));
+}
+
+export const addPromos = (promotions) => {
+    return ({
+        type: ActionTypes.ADD_PROMOS,
+        payload: promotions
+    });
+}
+
+export const PromosFailed = (errMess) => {
+    return({
+        type: ActionTypes.PROMOS_FAILED,
+        payload: errMess
+    });
+}
+
+export const promosLoading = () => {
+    return ({
+        type: ActionTypes.PROMOS_LOADING
+    });
+}
+
+export const fetchLeaders = () => (dispatch) => {
+    dispatch(leadersLoading());
+    return fetch(baseUrl + 'leaders' )
+                .then(response =>{
+                    if(response.ok){
+                        return response;
+                    }
+                    else{
+                        //server error
+                        let error = new Error("Error: " + response.status + ": " + response.statusText);
+                        error.response = response;
+                        throw error;
+                    }
+                },error => { //in app error
+                    let  errMess = new Error(error.message);
+                    throw errMess;
+                })
+                .then(response => response.json())
+                .then(leaders => dispatch(addLeaders(leaders)))
+                .catch(error => dispatch(leadersFailed(error.message)));
+}
+
+export const addLeaders = (leaders) => {
+    return ({
+        type: ActionTypes.ADD_LEADERS,
+        payload: leaders
+    });
+}
+
+export const leadersFailed = (errMess) => {
+    return({
+        type: ActionTypes.LEADERS_FAILED,
+        payload: errMess
+    });
+}
+
+export const leadersLoading = () => {
+    return ({
+        type: ActionTypes.LEADERS_LOADING
+    });
+}
